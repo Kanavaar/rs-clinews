9cbe651fbe50bdebef32a665d4f92bfd5f84076a
kanavaar <uju8765@gmail.com>
2023-04-05 22:17:02 +0200

    adding custom errors to newsapi library

--- newsapi/src/lib.rs
+++ newsapi/src/lib.rs
@@ -1,5 +1,14 @@
-use std::error::Error;
 use serde::Deserialize;
+
+#[derive(thiserror::Error, Debug)]
+pub enum NewsApiError {
+    #[error("Could not establish connection to newsapi")]
+    RequestFailed(Box<ureq::Error>),
+    #[error("Failed parsing respons to string")]
+    FailedParseString(Box<std::io::Error>),
+    #[error("Failed to create json from string")]
+    FailedJsonFromString(Box<serde_json::Error>),
+}
 
 #[derive(Deserialize, Debug)]
 pub struct Articles {
@@ -12,13 +21,15 @@ pub struct Article {
     pub url: String,
 }
 
-pub fn get_articles(url: &str, api_key: &str) -> Result<Articles, Box<dyn Error>> {
-    let res: String = ureq::get(&url)
-        .set("X-Api-Key", &api_key)
-        .call()?
-        .into_string()?;
+pub fn get_articles(url: &str, api_key: &str) -> Result<Articles, NewsApiError> {
+    let res: String = ureq::get(url)
+        .set("X-Api-Key", api_key)
+        .call()
+        .map_err(|e| NewsApiError::RequestFailed(Box::new(e)))?
+        .into_string()
+        .map_err(|e| NewsApiError::FailedParseString(Box::new(e)))?;
     
-    let articles: Articles = serde_json::from_str(&res)?;
+    let articles: Articles = serde_json::from_str(&res).map_err(|e| NewsApiError::FailedJsonFromString(Box::new(e)))?;
 
     Ok(articles)
 }
