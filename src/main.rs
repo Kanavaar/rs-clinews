79b4f7faf722873702f6743ace38dcc68f4e6619
kanavaar <uju8765@gmail.com>
2023-04-05 21:45:14 +0200

    extracting get_articles into a selfcontained crate

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,7 @@
 use std::error::Error;
-use serde::Deserialize;
 use colour::{dark_green, yellow};
 
-#[derive(Deserialize, Debug)]
-struct Articles {
-    articles: Vec<Article>
-}
-
-#[derive(Deserialize, Debug)]
-struct Article {
-    title: String,
-    url: String,
-}
-
-fn get_articles(url: &str, api_key: &str) -> Result<Articles, Box<dyn Error>> {
-    let res: String = ureq::get(&url)
-        .set("X-Api-Key", &api_key)
-        .call()?
-        .into_string()?;
-    
-    let articles: Articles = serde_json::from_str(&res)?;
-
-    Ok(articles)
-}
+use newsapi::{Articles, get_articles};
 
 fn render_articles(articles: &Articles) {
     for a in &articles.articles {
