f0ddb58c48d15acff1ab286889e7df721aa4f3c0
kanavaar <uju8765@gmail.com>
2023-04-06 17:52:22 +0200

    removing commented function

--- newsapi/src/lib.rs
+++ newsapi/src/lib.rs
@@ -36,20 +36,6 @@ pub struct Article {
     pub url: String,
 }
 
-// pub fn get_articles(url: &str, api_key: &str) -> Result<Articles, NewsApiError> {
-//     let res: String = ureq::get(url)
-//         .set("X-Api-Key", api_key)
-//         .call()
-//         .map_err(|e| NewsApiError::RequestFailed(Box::new(e)))?
-//         .into_string()
-//         .map_err(|e| NewsApiError::FailedParseString(Box::new(e)))?;
-    
-//     let articles: Articles = serde_json::from_str(&res)
-//         .map_err(|e| NewsApiError::FailedJsonFromString(Box::new(e)))?;
-
-//     Ok(articles)
-// }
-
 pub enum Endpoint {
     TopHeadlines,
 }
